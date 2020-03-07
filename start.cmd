96fac8038e5465fc4857a69740fd6631bd575646
zengliwei <zengliwei@163.com>
2020-03-07 22:57:34 +0800

    ^ small change for SSL

--- start.cmd
+++ start.cmd
@@ -95,7 +95,7 @@ goto :EOF
       endLocal
     )
     move ssl.conf.tmp ..\..\config\ssl\ext.cnf
-    docker exec dev_router /usr/bin/openssl req -x509 -nodes -sha256 -newkey rsa:2048 -outform PEM -days 3650 -addext "basicConstraints=critical, CA:true" -subj "/C=CN/ST=State/L=Locality/O=Organization/CN=Local Dev" -keyout ca.pvk -out /etc/ssl/certs/%domain%.browser.cer
+    docker exec dev_router /usr/bin/openssl req -x509 -nodes -sha256 -newkey rsa:2048 -outform PEM -days 3650 -addext "basicConstraints=critical, CA:true" -subj "/C=CN/ST=State/L=Locality/O=Organization/CN=Dev - %domain%" -keyout ca.pvk -out /etc/ssl/certs/%domain%.browser.cer
     docker exec dev_router /usr/bin/openssl req -nodes -sha256 -newkey rsa:2048 -out server.req -keyout /etc/ssl/certs/%domain%.pvk -subj "/C=CN/ST=State/L=Locality/O=Organization/CN=%domain%"
     docker exec dev_router /usr/bin/openssl x509 -req -sha256 -days 3650 -set_serial 0x1111 -extfile /etc/ssl/certs/ext.cnf -in server.req -CAkey ca.pvk -CA /etc/ssl/certs/%domain%.browser.cer -out /etc/ssl/certs/%domain%.cer
     docker exec dev_router rm ca.pvk server.req /etc/ssl/certs/ext.cnf
